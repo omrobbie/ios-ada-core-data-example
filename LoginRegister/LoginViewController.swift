b880051a62ca88bd9e73fc52d742dcbcf3d1cf64
omrobbie <putu.roby@yahoo.com>
2018-08-15 15:41:30 +0700

    koneksikan outlet dengan view login

--- LoginRegister/LoginViewController.swift
+++ LoginRegister/LoginViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func doSignIn(_ sender: Any) {
+    }
+    
 }
