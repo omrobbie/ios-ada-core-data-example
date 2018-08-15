e1a04519e9f42daafcbfd0b58250f8a2d7675a13
omrobbie <putu.roby@yahoo.com>
2018-08-15 16:12:11 +0700

    buat view controller untuk welcome page

---
+++ LoginRegister/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  LoginRegister
+//
+//  Created by omrobbie on 15/08/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
