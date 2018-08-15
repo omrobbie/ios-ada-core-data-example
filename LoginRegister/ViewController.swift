18111466908187df76df3c8da5aa96b98e6f7e43
omrobbie <putu.roby@yahoo.com>
2018-08-15 15:08:04 +0700

    koneksikan semua outlet di view register

--- LoginRegister/ViewController.swift
+++ LoginRegister/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtFullname: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func doRegister(_ sender: Any) {
+    }
+    
 }
 
